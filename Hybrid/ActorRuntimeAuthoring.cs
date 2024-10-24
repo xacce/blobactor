cbdb22ad1a1aaa3f20fc139e20d7354b14649edb
Xacce <mmodbrreciever@gmail.com>
2024-10-24 13:49:35 +0300

    runtime proxy

---
+++ Hybrid/ActorRuntimeAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using BlobActor.Runtime;
+using Unity.Entities;
+using UnityEngine;
+
+namespace BlobActor.Hybrid
+{
+    public class ActorRuntimeAuthoring : MonoBehaviour
+    {
+        private class ActorRuntimeBaker : Baker<ActorRuntimeAuthoring>
+        {
+            public override void Bake(ActorRuntimeAuthoring authoring)
+            {
+                var e = GetEntity(TransformUsageFlags.None);
+                AddComponent<ActorRuntime>(e);
+            }
+        }
+    }
+}
